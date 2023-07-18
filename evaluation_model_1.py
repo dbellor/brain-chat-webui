42cc42695a9eaa0c3ca8faa092abd5faed7a4585
dbellor <36899908+dbellor@users.noreply.github.com>
2023-07-18 15:06:31 -0400

    Create evaluation_model_1.py

---
+++ evaluation_model_1.py
@@ -0,0 +1,9 @@
+class EvaluationModel1:
+    def __init__(self):
+        # Initialize any necessary variables or models here
+        pass
+
+    def evaluate(self, processed_input, response):
+        # Evaluate the interaction between the InputModel and CentralModel here
+        # For now, this method just returns a placeholder string
+        return "Evaluation 1 result"
