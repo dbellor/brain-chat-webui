b39074ecafcd8606c50294e5898b3a368a6478be
dbellor <36899908+dbellor@users.noreply.github.com>
2023-07-20 12:15:04 -0400

    update main.py

--- app/main.py
+++ app/main.py
@@ -2,11 +2,17 @@ import json
 import logging
 import os
 from datetime import datetime
+import csv
 def main(input_model, central_model, output_model, evaluation_model_1, evaluation_model_2):
+    user_id = input("Enter your user ID: ")
     while True:
         user_input = input("Enter your message: ")
         if not user_input:
             continue
+        # Log the user input with the user ID
+        with open('user.csv', 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow([user_id, user_input])
         processed_input = input_model.process(user_input)
         try:
             response = central_model.generate_response(processed_input)
@@ -28,32 +34,38 @@ if __name__ == "__main__":
         print(f'Error reading logging_config.json file: {e}')
         config = {}
     logging.basicConfig(filename=config.get('filename', 'log.txt'), level=config.get('level', 'INFO'))
-    
+
+    # Create a CSV file to store user data if it doesn't exist
+    if not os.path.exists('user.csv'):
+        with open('user.csv', 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(['user_id', 'user_input'])
+
     input_model = None
     central_model = None
     output_model = None
     evaluation_model_1 = None
     evaluation_model_2 = None
-    
+
     if input_model is None or central_model is None or output_model is None or evaluation_model_1 is None or evaluation_model_2 is None:
         raise ValueError('One or more models are None')
-    
+
     main(input_model, central_model, output_model, evaluation_model_1, evaluation_model_2)
     central_model = os.environ.get('CENTRAL_MODEL')
     output_model = os.environ.get('OUTPUT_MODEL')
     evaluation_model_1 = os.environ.get('EVALUATION_MODEL_1')
     evaluation_model_2 = os.environ.get('EVALUATION_MODEL_2')
-    
+
     if input_model is None or central_model is None or output_model is None or evaluation_model_1 is None or evaluation_model_2 is None:
         raise ValueError('One or more models are None')
-    
+
     main(input_model, central_model, output_model, evaluation_model_1, evaluation_model_2)
     if input_model is None or central_model is None or output_model is None or evaluation_model_1 is None or evaluation_model_2 is None:
         raise ValueError('One or more models are None')
-    
+
     main(input_model, central_model, output_model, evaluation_model_1, evaluation_model_2)
-    
+
     if input_model is None or central_model is None or output_model is None or evaluation_model_1 is None or evaluation_model_2 is None:
         raise ValueError('One or more models are None')
-    
+
     main(input_model, central_model, output_model, evaluation_model_1, evaluation_model_2)
